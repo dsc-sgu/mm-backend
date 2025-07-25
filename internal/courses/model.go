2208431137f97f1a01bee8c9e0358dade26c0f98
ProstoLive <artem009saratov@gmail.com>
2025-07-25 15:42:41 +0400

    feat: added struct tags to course models

--- internal/courses/model.go
+++ internal/courses/model.go
@@ -11,22 +11,22 @@ type CourseID struct {
 }
 
 type CourseType struct {
-	Id           uuid.UUID
-	DisciplineId uuid.UUID
-	OwnerId      uuid.UUID
-	Name         string
-	CreatedAt    time.Time
+	Id           uuid.UUID `json:"id" db:"id" binding:"required"`
+	DisciplineId uuid.UUID `json:"disciplineId" db:"discipline_id"`
+	OwnerId      uuid.UUID `json:"ownerId" db:"owner_id" binding:"required"`
+	Name         string    `json:"name" db:"name" binding:"required"`
+	CreatedAt    time.Time `json:"createdAt" db:"created_at" binding:"required"`
 }
 
 type CreateCourseType struct {
-	DisciplineId uuid.UUID
-	Name         string
+	DisciplineId uuid.UUID `json:"disciplineId" db:"discipline_id"`
+	Name         string    `json:"name" db:"name" binding:"required"`
 }
 
 type UpdateCourseType struct {
 	CourseID
-	OwnerId uuid.UUID
-	Name    string
+	OwnerId uuid.UUID `json:"ownerId" db:"owner_id"`
+	Name    string    `json:"name" db:"name"`
 }
 
 type CoursePagination struct {
