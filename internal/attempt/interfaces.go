c15ed63b293dc905d40f794285668ba96c88df46
Ксения Иванова <natimal.well@gmail.com>
2025-08-02 18:20:26 +0400

    Add models for attempts and interface for work with Repository

---
+++ internal/attempt/interfaces.go
@@ -0,0 +1,19 @@
+package attempt
+
+import "github.com/google/uuid"
+
+type RepoManager interface {
+	InitRepo(repoID RepoID) error
+	RemoveRepo(repoID RepoID) error
+	MakeAttempt(repoID RepoID, fileInfo []FileInfo) (attempt Attempt, err error)
+	GetAttempts(repoID RepoID) ([]Attempt, error)
+	GetDiff(attemptID1, attemptID2 uuid.UUID) ([]string, error)
+}
+
+type FileDescriptor = string
+
+type FileStorage interface {
+	StoreFile(fileInfo FileInfo) (FileInfo, error)
+	GetFile(desc FileDescriptor) (FileInfo, error)
+	RemoveFile(desc FileDescriptor) error
+}
