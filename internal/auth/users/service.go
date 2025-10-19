ac8d686913642ecb1b004cf9ff788133a310e202
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-10-20 02:02:48 +0400

    feat: implement user service

--- internal/auth/users/service.go
+++ internal/auth/users/service.go
@@ -1,9 +1,31 @@
 package users
 
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
 type Service struct {
 	repo Repo
 }
 
 func NewService(repo Repo) *Service {
 	return &Service{repo}
+}
+
+func (s *Service) GetUserByEmail(ctx context.Context, email string) (*Model, error) {
+  return s.repo.GetUserByEmail(ctx, email)
+}
+
+func (s *Service) CreateUser(user *CreateModel) (*Model, error) {
+  return s.repo.CreateUser(user)
+}
+
+func (s *Service) GetUserById(ctx context.Context, id uuid.UUID) (*Model, error) {
+  return s.repo.GetUserById(ctx, id)
+}
+
+func (s *Service) DeleteUserById(id uuid.UUID) error {
+  return s.repo.DeleteUserById(id)
 }
