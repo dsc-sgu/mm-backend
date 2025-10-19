5eb5ef54e24f1f252deab650cd5e0b07c51c77c0
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-10-20 02:27:51 +0400

    refactor: move interfaces checking

--- internal/pg/pg.go
+++ internal/pg/pg.go
@@ -1,7 +1,10 @@
 package pg
 
 import (
-  "github.com/MergeMinds/mm-backend-go/internal/auth/users"
+	"github.com/MergeMinds/mm-backend-go/internal/auth/users"
+	"github.com/MergeMinds/mm-backend-go/internal/blocks"
+	"github.com/MergeMinds/mm-backend-go/internal/courses"
+	"github.com/MergeMinds/mm-backend-go/internal/disciplines"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -13,4 +16,8 @@ func NewPGRepo(db *sqlx.DB) *PGRepo {
 	return &PGRepo{db}
 }
 
+// Check interfaces
 var _ users.Repo = (*PGRepo)(nil)
+var _ blocks.Repo = (*PGRepo)(nil)
+var _ courses.Repo = (*PGRepo)(nil)
+var _ disciplines.Repo = (*PGRepo)(nil)
