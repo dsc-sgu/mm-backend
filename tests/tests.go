746ab465222d6f7617f6304496e59bd5f93a5ad6
Aljel <makakwar@gmail.com>
2025-11-12 16:17:28 +0400

    feat: initBackend func

--- tests/tests.go
+++ tests/tests.go
@@ -11,12 +11,37 @@ import (
 )
 
 func initBackend(
+	ctx context.Context,
 	t *testing.T,
 	net *testcontainers.DockerNetwork,
-) (nat.Port, error) {
-	// 1. initialize container request
-	// 2. create request using GenericContainer
-	// 3. call function which will clean up container after test
+) (*nat.Port, error) {
+	basePort := "8013"
+	
+	req, err := testcontainers.ContainerRequest{
+		Image: "mm-backend",
+		ExposedPorts: []string{basePort + "/tcp"},
+		WaitingFor: wait.ForListeningPort(basePort + "/tcp"),
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	container, err := testcontainers.GenericContainer(
+		ctx,
+		testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started: true,
+		}
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	testcontainers.CleanupContainer(t, container)
+
+	return container.MappedPort(ctx, basePort)
 }
 
 func initPostgres(
@@ -36,8 +61,7 @@ func readBodyToMap(rc io.ReadCloser) (map[string]any, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer func() {
-		_ = rc.Close()
+	defer func() { _ = rc.Close()
 	}()
 
 	var result map[string]any
