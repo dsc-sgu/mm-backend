a62b6b8a7642a630a1374cb94f829f07212f4326
Ezhkin-Kot <ezkot.tokze@gmail.com>
2026-01-06 17:53:23 +0400

    feat: add some tests

--- tests/course_test.go
+++ tests/course_test.go
@@ -2,12 +2,18 @@ package tests
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go/network"
+
+	"github.com/dsc-sgu/mm-backend/internal/courses"
 )
 
+// Tests for courses
 func TestCreateCourse(t *testing.T) {
 	ctx := context.Background()
 
@@ -47,4 +53,149 @@ func TestCreateCourse(t *testing.T) {
 		"Test Course",
 	)
 	require.NotZero(t, courseID)
+}
+
+func TestGetCourseByID(t *testing.T) {
+	ctx := context.Background()
+
+	net, err := network.New(ctx)
+	require.NoError(t, err)
+
+	_, err = initPostgres(ctx, t, net)
+	require.NoError(t, err)
+
+	port, err := initBackend(ctx, t, net)
+	require.NoError(t, err)
+
+	userID := CreateTestUser(
+		t,
+		port,
+		"Test First Name",
+		"Test Last Name",
+		"Username",
+		"test@email.com",
+		"password",
+	)
+	require.NotZero(t, userID)
+
+	disciplineID := CreateTestDiscipline(
+		t,
+		port,
+		userID,
+		"Test Discipline",
+	)
+	require.NotZero(t, disciplineID)
+
+	courseID := CreateTestCourse(
+		t,
+		port,
+		userID,
+		disciplineID,
+		"Test Course",
+	)
+	require.NotZero(t, courseID)
+
+	url := fmt.Sprintf(
+		"http://localhost:%s/api/v1/courses/%s?fake_user_id=%s",
+		port.Port(),
+		courseID,
+		userID,
+	)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	require.NoError(t, err)
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+
+	var recievedCourse courses.Course
+	require.NoError(t, json.NewDecoder(resp.Body).Decode(&recievedCourse))
+
+	require.Equal(t, courseID, recievedCourse.Id)
+	require.Equal(t, "Test Course", recievedCourse.Name)
+	require.Equal(t, disciplineID, recievedCourse.DisciplineId)
+}
+
+func TestGetPaginatedCourse(t *testing.T) {
+	ctx := context.Background()
+
+	net, err := network.New(ctx)
+	require.NoError(t, err)
+
+	_, err = initPostgres(ctx, t, net)
+	require.NoError(t, err)
+
+	port, err := initBackend(ctx, t, net)
+	require.NoError(t, err)
+
+	userID := CreateTestUser(
+		t,
+		port,
+		"Test First Name",
+		"Test Last Name",
+		"Username",
+		"test@email.com",
+		"password",
+	)
+	require.NotZero(t, userID)
+
+	disciplineID := CreateTestDiscipline(
+		t,
+		port,
+		userID,
+		"Test Discipline",
+	)
+	require.NotZero(t, disciplineID)
+
+	for i := range 5 {
+		id := CreateTestCourse(
+			t,
+			port,
+			userID,
+			disciplineID,
+			fmt.Sprintf("Course %d", i),
+		)
+		require.NotZero(t, id)
+	}
+
+	limit := 2
+	offset := 1
+
+	url := fmt.Sprintf(
+		"http://localhost:%s/api/v1/courses?limit=%d&offset=%d&fake_user_id=%s",
+		port.Port(),
+		limit,
+		offset,
+		userID,
+	)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	require.NoError(t, err)
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+
+	var recievedCourses []*courses.Course
+	require.NoError(t, json.NewDecoder(resp.Body).Decode(&recievedCourses))
+
+	require.Len(t, recievedCourses, limit)
+
+	require.Equal(t, "Course 1", recievedCourses[0].Name)
+	require.Equal(t, "Course 2", recievedCourses[1].Name)
 }
