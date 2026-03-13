58cc5d3e42f702f80397e46ffc8caea480491f2d
Aljel <makakwar@gmail.com>
2026-03-13 19:12:31 +0400

    feat: add tests

--- internal/pg/courses.go
+++ internal/pg/courses.go
@@ -267,7 +267,7 @@ func (r *PGRepo) GetPaginatedCourses(
 		SELECT id, discipline_id, owner_id, name, created_at
 		FROM courses
 		%s
-		ORDER BY id
+		ORDER BY name
 		LIMIT $1
 	`, whereClause)
 
