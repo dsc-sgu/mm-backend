b4750dd599e7082e66ebe3d131a38533e72913cd
Ezhkin-Kot <ezkot.tokze@gmail.com>
2026-06-15 16:51:05 +0400

    feat: add course lock models and interface methods

--- internal/courses/repo.go
+++ internal/courses/repo.go
@@ -46,14 +46,6 @@ type CoursePagination struct {
 	LastID uuid.UUID `query:"last_id"`
 }
 
-// CourseLock is the database representation of a course lock.
-type CourseLock struct {
-	CourseID  uuid.UUID `json:"courseID"  db:"course_id"  binding:"required"`
-	UserID    uuid.UUID `json:"userID"    db:"user_id"    binding:"required"`
-	SessionID uuid.UUID `json:"sessionID" db:"session_id" binding:"required"`
-	ExpiresAt time.Time `json:"expiresAt" db:"expires_at" binding:"required"`
-}
-
 // CourseMember is the database representation of a course member.
 type CourseMember struct {
 	UserID    uuid.UUID        `json:"userID"    db:"user_id"`
