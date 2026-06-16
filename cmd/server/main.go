b45d9837ce256c31e1795ee5d2dc0b3376ddab52
Ezhkin-Kot <ezkot.tokze@gmail.com>
2026-06-16 21:50:25 +0400

    feat: initialize new services in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -29,11 +29,13 @@ import (
 	"github.com/dsc-sgu/mm-backend/internal/blocks"
 	"github.com/dsc-sgu/mm-backend/internal/config"
 	"github.com/dsc-sgu/mm-backend/internal/courses"
+	"github.com/dsc-sgu/mm-backend/internal/courses/locks"
 	"github.com/dsc-sgu/mm-backend/internal/db"
 	"github.com/dsc-sgu/mm-backend/internal/disciplines"
 	"github.com/dsc-sgu/mm-backend/internal/git"
 	"github.com/dsc-sgu/mm-backend/internal/logger"
 	"github.com/dsc-sgu/mm-backend/internal/pg"
+	"github.com/dsc-sgu/mm-backend/internal/snapshots"
 	pkggit "github.com/dsc-sgu/mm-backend/pkg/git"
 )
 
@@ -164,14 +166,16 @@ func main() {
 	pgRepo := pg.NewPGRepo(dbConn)
 
 	blockService := blocks.NewService(pgRepo)
-	courseService := courses.NewService(pgRepo)
+	snapshotService := snapshots.NewService(pgRepo)
+	lockService := locks.NewService(pgRepo)
+	courseService := courses.NewService(pgRepo, snapshotService, lockService, pgRepo)
 	disciplineService := disciplines.NewService(pgRepo)
 	userService := users.NewService(pgRepo, sessionRepo, cookieConfig)
 	gitService := git.NewService(pgRepo)
 
 	userHandler := users.NewHandler(userService)
-	blockHandler := blocks.NewHandler(blockService)
-	courseHandler := courses.NewHandler(courseService, blockService)
+	blockHandler := blocks.NewHandler(blockService, pgRepo, lockService)
+	courseHandler := courses.NewHandler(courseService, blockService, lockService, snapshotService)
 	disciplineHandler := disciplines.NewHandler(disciplineService)
 	gitHandler := git.NewHandler(gitService)
 
